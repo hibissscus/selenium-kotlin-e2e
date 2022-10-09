64cdb3029086bd177a192d0c356eaf1e3723e0a9
hibissscus <hibissscus@gmail.com>
2022-10-10 00:57:50 +0200

    .space.kts uploading report of e2e test results

--- .space.kts
+++ .space.kts
@@ -47,17 +47,18 @@ job("Build and run e2e tests using docker") {
                 echo SPACE_WORKER_HOSTNAME ${'$'}SPACE_WORKER_HOSTNAME
             """
         }
-//        shellScript {
-//            // SOURCE_PATH is path to the build artifact
-//            // TARGET_PATH is the destination path in the file repository
-//            // Note that each run of the build script creates a separate directory (name = build number)
-//            content = """
-//                echo Uploading report of e2e test results...
-//                SOURCE_PATH=@tmp/jetbrains/space/automation/worker/data/steps-2EaVxe21Mqwv-2EaVxe21Mqwv-1/step-data-2EaVxe21Mqwv/work/selenium-kotlin-e2e/build/reports/tests/docker/e2e/index.html
-//                TARGET_PATH=/${'$'}JB_SPACE_EXECUTION_NUMBER/
-//                REPO_URL=https://files.pkg.jetbrains.space/e2e/p/e2e/e2e-test-reports
-//                curl -i -H "Authorization: Bearer ${'$'}JB_SPACE_CLIENT_TOKEN" -F file=@"${'$'}SOURCE_PATH" ${'$'}REPO_URL/${'$'}TARGET_PATH
-//            """
-//        }
+
+        shellScript {
+            // SOURCE_PATH is path to the build artifact
+            // TARGET_PATH is the destination path in the file repository
+            // Each run of the build script creates a separate directory (name = build number)
+            content = """
+                echo Uploading report of e2e test results...
+                SOURCE_PATH=${'$'}JB_SPACE_WORK_DIR_PATH/build/reports/tests/docker/e2e/e2e.png
+                TARGET_PATH=/${'$'}JB_SPACE_EXECUTION_NUMBER/
+                REPO_URL=https://files.pkg.jetbrains.space/e2e/p/e2e/e2e-test-reports
+                curl -i -H "Authorization: Bearer ${'$'}JB_SPACE_CLIENT_TOKEN" -F file=@"${'$'}SOURCE_PATH" ${'$'}REPO_URL/${'$'}TARGET_PATH
+            """
+        }
     }
 }
