be57b0a500e40f325cbe61796f4c2a90ad6a1261
hibissscus <hibissscus@gmail.com>
2022-10-09 22:08:32 +0200

    .space.kts

--- .space.kts
+++ .space.kts
@@ -1,13 +1,12 @@
 /**
-* JetBrains Space Automation
-* This Kotlin-script file lets you automate build activities
-* For more info, see https://www.jetbrains.com/help/space/automation.html
-*/
+ * JetBrains Space Automation
+ * see https://www.jetbrains.com/help/space/automation.html
+ */
 
 job("Build and run e2e tests in docker") {
-    container(displayName = "Gradle build", image = "openjdk:11") {
-        kotlinScript { api ->
-            api.gradlew("docker")
+    container(image = "gradle") {
+        shellScript {
+            content = "gradle docker"
         }
     }
 }
