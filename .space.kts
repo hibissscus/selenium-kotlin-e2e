a96920f4b1c7e373de66d4c6f6bff341e5fccfb2
hibissscus <hibissscus@gmail.com>
2022-10-10 20:33:26 +0200

    .space.kts uploading report of e2e test results

--- .space.kts
+++ .space.kts
@@ -26,11 +26,12 @@ job("Build and run e2e tests using docker") {
                 echo Build and run e2e tests using docker...
                 ./gradlew docker
                 echo Uploading report of e2e test results...
-                echo SOURCE_PATH=${'$'}JB_SPACE_WORK_DIR_PATH/build/reports/tests/docker/e2e/e2e.png
-                echo TARGET_PATH=/${'$'}JB_SPACE_EXECUTION_NUMBER/
-                echo REPO_URL=https://files.pkg.jetbrains.space/e2e/p/e2e/e2e-test-reports
-                echo file="${'$'}SOURCE_PATH" ${'$'}REPO_URL/${'$'}TARGET_PATH
-                echo target=${'$'}REPO_URL/${'$'}TARGET_PATH
+                SOURCE_PATH=${'$'}JB_SPACE_WORK_DIR_PATH/build/reports/tests/docker/e2e/e2e.png
+                TARGET_PATH=/${'$'}JB_SPACE_EXECUTION_NUMBER/
+                REPO_URL=https://files.pkg.jetbrains.space/e2e/p/e2e/e2e-test-reports
+                echo SOURCE_PATH=${'$'}SOURCE_PATH
+                echo TARGET_PATH=${'$'}TARGET_PATH
+                echo REPO_URL=${'$'}REPO_URL
                 curl -i -H "Authorization: Bearer ${'$'}JB_SPACE_CLIENT_TOKEN" -F file=@"${'$'}SOURCE_PATH" ${'$'}REPO_URL/${'$'}TARGET_PATH
             """
         }
