3d505fbccecfb1db3d7fa03b648e78118c6c99fb
hibissscus <hibissscus@gmail.com>
2022-10-10 19:58:59 +0200

    .space.kts uploading report of e2e test results

--- .space.kts
+++ .space.kts
@@ -23,17 +23,13 @@ job("Build and run e2e tests using docker") {
     host {
         shellScript {
             content = """
-                echo Spin up selenium chrome nodes in docker...
-                docker-compose up chrome
                 echo Build and run e2e tests using docker...
-                gradle docker 
+                ./gradlew docker
                 echo Uploading report of e2e test results...
                 echo SOURCE_PATH=${'$'}JB_SPACE_WORK_DIR_PATH/build/reports/tests/docker/e2e/e2e.png
                 echo TARGET_PATH=/${'$'}JB_SPACE_EXECUTION_NUMBER/
                 echo REPO_URL=https://files.pkg.jetbrains.space/e2e/p/e2e/e2e-test-reports
                 curl -i -H "Authorization: Bearer ${'$'}JB_SPACE_CLIENT_TOKEN" -F file=@"${'$'}SOURCE_PATH" ${'$'}REPO_URL/${'$'}TARGET_PATH
-                echo Switch off docker containers...
-                docker-compose down --remove-orphans
             """
         }
     }
